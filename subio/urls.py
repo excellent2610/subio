e76f10f087271a4c2e0893a4ca7d2361ca3bc57c
excellent2610 <shkvotersb@gmail.com>
2026-01-28 14:15:55 +0200

    Змінив маршрут головної сторінки

--- subio/urls.py
+++ subio/urls.py
@@ -6,13 +6,16 @@ from django.conf.urls.static import static
 from django.shortcuts import redirect
 
 urlpatterns = [
+    # Головна сторінка тепер веде на dashboard
+    path("", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
+    
+    # Інші додатки
     path("users/", include(("users.urls", "users"), namespace="users")),
     path("subscriptions/", include(("subscriptions.urls", "subscriptions"), namespace="subscriptions")),
-    path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
     path('notifications/', include('notifications.urls')),
-    path('users/', include('users.urls')),
-    path('subscriptions/', include('subscriptions.urls')),
-    path('notifications/', include('notifications.urls')),
+    
+    # Адмінка та фавікон
+    path("admin/", admin.site.urls),
     path(
         'favicon.ico',
         RedirectView.as_view(
@@ -20,7 +23,8 @@ urlpatterns = [
             permanent=True
         )
     ),
-    path("admin/", admin.site.urls),
-    path("", lambda request: redirect("users:login")),
-
 ]
+
+# Додаємо обслуговування статичних файлів, якщо це розробка
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
